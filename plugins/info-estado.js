3bb9002ec20ced15948a4141f20d9fef70fe3016
Durl.Dev <92341694+Durl99@users.noreply.github.com>
2024-08-19 12:41:59 -0600

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -14,31 +14,31 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
-let estado = `╭━━━━[ *𝙀𝙎𝙏𝘼𝘿𝙊 | 𝙎𝙏𝘼𝙏𝙐𝙎* ]━━━━━⬣
-┃💗 *¡Hola | Hi!* ${name}
+let estado = `╭━━━━[ 🅴🆂🆃🅰🅳🅾 ]━━━━━⬣
+┃👋    *Hola!* ${name}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃𓃠 *Versión de ${gt}*
-┃➥ ${vs}
+┃🔮 *Versión de ${gt}*
+┃★ ${vs}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✿ 𝗖𝗥𝗘𝗔𝗗𝗢𝗥𝗔 | 𝗖𝗥𝗘𝗔𝗧𝗢𝗥
-┃ღ 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨
+┃💻 CREADOR
+┃★ DURL.DEV
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✿ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧𝗢 | 𝗖𝗢𝗡𝗧𝗔𝗖𝗧
-┃➥ *${ig}*
+┃☎️ CONTACTO
+┃★ *${ig}*
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ღ ${global.opts['self'] ? '𝙋𝙍𝙄𝙑𝘼𝘿𝙊 - 𝙋𝙍𝙄𝙑𝘼𝙏𝙀' : '𝙋𝙐𝘽𝙇𝙄𝘾𝙊 - 𝙋𝙐𝘽𝙇𝙄𝘾'}
+┃★ ${global.opts['self'] ? 'PRIVADO' : 'PUBLICO'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ღ *Activo Durante | Active During* 
-┃➥ ${uptime}
-┃ღ *Usuario(s) | Users* 
-┃➥ ${Object.keys(global.db.data.users).length} 
-┃ღ *Chat(s) Prohibido(s) | Forbidden Chats*
-┃➥ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
-┃ღ *Usuario(s) Prohibido(s) | Prohibited Urs*
-┃➥ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
+┃★ *TIEMPO ACTIVO † ACTIVE TIME * 
+┃» ${uptime}
+┃★ *Usuario(s) | Users* 
+┃» ${Object.keys(global.db.data.users).length} 
+┃★ *Chat(s) Baneados(s) | Forbidden Chats*
+┃» ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
+┃★ *Usuario(s) Baneados(s) | Prohibited Urs*
+┃» ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
 ╰━━━━━━━━━━━━━━━━━━⬣`
 await conn.reply(m.chat, estado, m, fakeChannel)
-//await conn.sendButton(m.chat, estado, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n` + wm, pp, [
+//await conn.sendButton(m.chat, estado, `𝔸𝕊𝕀𝕊𝕋𝔼ℕℂ𝕀𝔸\n${asistencia}\n\n` + wm, pp, [
 //['𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪', '.allmenu'],
 //['𝙑𝙚𝙡𝙤𝙘𝙞𝙙𝙖𝙙 | 𝙎𝙥𝙚𝙚𝙙', '/ping'],
 //['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪', '#menu']], null, [
